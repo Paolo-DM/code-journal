0b27e06f521ec2c82201e1769e2c517a68f99cc1
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-04-04 23:06:17 +0000

    Added functionality to save entries to local storage and completed issue-1-create-an-entry.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,26 @@
 /* exported data */
 var $photoUrl = document.querySelector('#photo-url');
 var $photo = document.querySelector('.photo');
+var $form = document.querySelector('form');
+var $title = document.querySelector('#title');
+var $notes = document.querySelector('#notes');
 
 $photoUrl.addEventListener('input', function () {
   $photo.setAttribute('src', $photoUrl.value);
 });
+
+$form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var entryValues = {
+    title: $title.value,
+    photoUrl: $photoUrl.value,
+    notes: $notes.value,
+    nextEntryId: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.unshift(entryValues);
+  $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
+}
