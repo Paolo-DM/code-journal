bf49c883a9becdbb1af542bca89f292cae9c2654
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-04-04 21:00:21 +0000

    Listen for input events to update the src attribute of the photo preview.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 /* global data */
 /* exported data */
+var $photoUrl = document.querySelector('#photo-url');
+var $photo = document.querySelector('.photo');
+
+$photoUrl.addEventListener('input', function () {
+  $photo.setAttribute('src', $photoUrl.value);
+});
