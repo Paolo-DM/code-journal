b644468c0e3394a17af75a47abc40c29cf56b73f
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-04-05 12:33:01 +0000

    Used a loop to create a DOM tree for each journal entry in the data model and append it to the page and removed the example HTML entry.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ var $photo = document.querySelector('.photo');
 var $form = document.querySelector('form');
 var $title = document.querySelector('#title');
 var $notes = document.querySelector('#notes');
+var $ul = document.querySelector('.ul-list');
+
+$photoUrl.addEventListener('input', function () {
+  $photo.setAttribute('src', $photoUrl.value);
+});
 
 $photoUrl.addEventListener('input', function () {
   $photo.setAttribute('src', $photoUrl.value);
@@ -25,3 +30,45 @@ function handleSubmit(event) {
   $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 }
+
+/*
+<ul class="row">
+        <li>
+          <img class="column-half" src="images/placeholder-image-square.jpg">
+          <div class="column-half">
+            <h2>Ada Lovelace</h2>
+            <p>Augusta Ada King, Countess of Lovelace (née Byron; 10 December
+              1815 – 27 November 1852) was an English mathematician and
+              writer, chiefly known for her work on Charles Babbage's proposed
+              mechanical general-purpose computer, the Analyticaler.</p>
+          </div>
+        </li>
+      </ul>
+*/
+
+function renderEntry(entry) {
+  var $li = document.createElement('li');
+  var $photoEl = document.createElement('img');
+  $photoEl.setAttribute('class', 'column-half');
+  $photoEl.setAttribute('src', entry.photoUrl);
+  var $colHalfDiv = document.createElement('div');
+  $colHalfDiv.setAttribute('class', 'column-half');
+  var $h2Title = document.createElement('h2');
+  $h2Title.textContent = entry.title;
+  var $pNotes = document.createElement('p');
+  $pNotes.textContent = entry.notes;
+
+  $ul.appendChild($li);
+  $li.appendChild($photoEl);
+  $li.appendChild($colHalfDiv);
+  $colHalfDiv.appendChild($h2Title);
+  $colHalfDiv.appendChild($pNotes);
+}
+
+function createDomTree() {
+  for (var entry of data.entries) {
+    renderEntry(entry);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', createDomTree);
