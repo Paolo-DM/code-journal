a6449ae7fd7f4dfb59b44c0b11357a58e7f0cb37
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-04-06 22:00:44 +0000

    Updated the entry form's submit handler to contitionally add or update a new entry object and to add or replace the existing DOM tree.

--- js/main.js
+++ js/main.js
@@ -12,13 +12,6 @@ var $modal = document.querySelector('.modal');
 var $entries = document.querySelector('.entries');
 var $newEntry = document.querySelector('.new-entry');
 
-function assignEntryId() {
-  for (var i = data.entries.length - 1; i >= 0; i--) {
-    data.entries[i].entryId = i;
-    data.entries[i].nextEntryId = i + 1;
-  }
-}
-
 $photoUrl.addEventListener('input', function () {
   $photo.setAttribute('src', $photoUrl.value);
 });
@@ -30,23 +23,36 @@ $photoUrl.addEventListener('input', function () {
 $form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
-
   event.preventDefault();
   var entryValues = {
     title: $title.value,
     photoUrl: $photoUrl.value,
-    notes: $notes.value
-    // nextEntryId: data.nextEntryId
-
+    notes: $notes.value,
+    entryId: data.nextEntryId
   };
-  // data.nextEntryId++;
-  data.entries.unshift(entryValues);
-  $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
-  $form.reset();
-  $modal.classList.add('hidden');
-  $ul.prepend(renderEntry(entryValues));
+
+  if (data.editing === null) {
+    data.nextEntryId++;
+    data.entries.unshift(entryValues);
+    $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
+    $ul.prepend(renderEntry(entryValues));
+  } else {
+    data.editing.title = $form.elements.title.value;
+    data.editing.photoUrl = $form.elements['photo-url'].value;
+    data.editing.notes = $form.notes.value;
+
+    var $liEntries = document.querySelectorAll('li');
+    for (var li of $liEntries) {
+      var liEntryId = Number(li.getAttribute('data-entry-id'));
+      if (data.editing.entryId === liEntryId) {
+        li.replaceWith(renderEntry(data.editing));
+      }
+    }
+    data.editing = null;
+  }
   showEntries();
-  assignEntryId();
+  hideModal();
+  $form.reset();
 }
 
 function renderEntry(entry) {
@@ -55,17 +61,23 @@ function renderEntry(entry) {
   var $li = document.createElement('li');
   $li.setAttribute('class', 'row');
   $li.setAttribute('data-entry-id', entry.entryId);
+
   var $photoEl = document.createElement('img');
   $photoEl.setAttribute('class', 'column-half');
   $photoEl.setAttribute('src', entry.photoUrl);
+
   var $colHalfDiv = document.createElement('div');
   $colHalfDiv.setAttribute('class', 'column-half');
+
   var $divSpcBtwn = document.createElement('div');
   $divSpcBtwn.setAttribute('class', 'space-between');
+
   var $h2Title = document.createElement('h2');
   $h2Title.textContent = entry.title;
+
   var $editIcon = document.createElement('i');
   $editIcon.setAttribute('class', 'fas fa-pen');
+
   var $pNotes = document.createElement('p');
   $pNotes.textContent = entry.notes;
 
@@ -82,35 +94,49 @@ function renderEntry(entry) {
 function createDomTree() {
   if (data.entries.length) {
     for (var entry of data.entries) {
-      var $ulEntry = renderEntry(entry);
-      $ul.appendChild($ulEntry);
+      var $liEntry = renderEntry(entry);
+      $ul.appendChild($liEntry);
     }
   }
 }
 
-$ul.addEventListener('click', function (event) {
-  if (event.target.matches('i')) {
-    const currentIndex = event.target.closest('li').getAttribute('data-entry-id');
-    $form.elements.title.value = data.entries[currentIndex].title;
-    $photo.setAttribute('src', data.entries[currentIndex].photoUrl);
-    $form.elements['photo-url'].value = data.entries[currentIndex].photoUrl;
-    $form.notes.value = data.entries[currentIndex].notes;
-    data.editing = data.entries[currentIndex];
-    hideEntries();
-    $newEntry.textContent = 'Edit Entry';
-    showModal();
-  }
-});
-
 window.addEventListener('DOMContentLoaded', createDomTree);
+
+$ul.addEventListener('click', editEntry);
 
 $newFormBtn.addEventListener('click', function (event) {
   hideEntries();
   showModal();
 });
 
+function editEntry(event) {
+  if (event.target.matches('i')) {
+    var closestLi = event.target.closest('li');
+    var currentIndex = Number(closestLi.getAttribute('data-entry-id'));
+
+    hideEntries();
+    showModal();
+    $newEntry.textContent = 'Edit Entry';
+
+    for (var entry of data.entries) {
+      if (currentIndex === Number(entry.entryId)) {
+        data.editing = entry;
+      }
+    }
+
+    $form.elements.title.value = data.editing.title;
+    $photo.setAttribute('src', data.editing.photoUrl);
+    $form.elements['photo-url'].value = data.editing.photoUrl;
+    $form.notes.value = data.editing.notes;
+  }
+}
+
 function showModal() {
   $modal.classList.remove('hidden');
+}
+
+function hideModal() {
+  $modal.classList.add('hidden');
 }
 
 function hideEntries() {
